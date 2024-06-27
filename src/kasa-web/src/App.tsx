991f43456f68fb5b96a69250de1ee543429f1aac
Yuliya BERT <98463848+ElkaXX@users.noreply.github.com>
2024-06-27 19:22:30 +0200

    Added pages

--- src/kasa-web/src/App.tsx
+++ src/kasa-web/src/App.tsx
@@ -1,10 +1,18 @@
-import "./app.scss"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage";
+import AboutPage from "./pages/About/AboutPage";
+import NotFoundPage from "./pages/NotFound/NotFoundPage";
 
 function App() {
-
   return (
-    <div className="app"></div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
