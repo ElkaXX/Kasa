6a4ef3afccb1f68a6032b109a5b2a589e6f6ee98
Yuliya BERT <98463848+ElkaXX@users.noreply.github.com>
2024-06-28 14:09:36 +0200

    Added variabled and footer

--- src/kasa-web/src/components/Footer/Footer.tsx
+++ src/kasa-web/src/components/Footer/Footer.tsx
@@ -1,5 +1,15 @@
+import "./Footer.scss";
+import logo from "../../assets/LOGO.svg";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <img src={logo} alt="logo" className="footer__logo" />
+        <div className="footer__copy">© 2020 Kasa. All rights reserved</div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
