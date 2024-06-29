a1339d4224de6bc14cf3fc4339abd185b937e6c3
Yuliya BERT <98463848+ElkaXX@users.noreply.github.com>
2024-06-29 09:04:29 +0200

    Added expander and about page

--- src/kasa-web/src/components/Expander/Expander.tsx
+++ src/kasa-web/src/components/Expander/Expander.tsx
@@ -1,5 +1,42 @@
-const Expander = () => {
-  return <div>Expander</div>;
+import React, { useState } from "react";
+import arrow from "../../assets/arrow.svg";
+import "./Expander.scss";
+
+type Props = {
+  title: string;
+  children?: React.ReactNode;
+  className?: string;
+};
+
+const Expander = ({ title, className, children }: Props) => {
+  const [expanded, setExpanded] = useState<boolean>(false);
+
+  return (
+    <div className={"expander " + className}>
+      <div className="expander__header" onClick={() => setExpanded(!expanded)}>
+        <div className="expander__title">{title}</div>
+        <img
+          src={arrow}
+          alt="arrow"
+          className={
+            expanded
+              ? "expander__arrow expander__arrow_expanded"
+              : "expander__arrow"
+          }
+        />
+      </div>
+
+      <div
+        className={
+          expanded
+            ? "expander__content expander__content_expanded"
+            : "expander__content"
+        }
+      >
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Expander;
--- src/kasa-web/src/pages/About/AboutPage.tsx
+++ src/kasa-web/src/pages/About/AboutPage.tsx
@@ -1,5 +1,69 @@
+import Expander from "../../components/Expander/Expander";
+import "./AboutPage.scss";
+
+// const expandersData = [
+//   {
+//     title: "Fiabilité",
+//     content:
+//       ,
+//   },
+//   {
+//     title: "Respect",
+//   },
+//   {
+//     title: "Service",
+//   },
+//   {
+//     title: "Sécurité",
+//   },
+// ];
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className="about">
+      <div className="about__banner">
+        <div className="about__overlay"></div>
+      </div>
+
+      <div className="about__wrapper">
+        <Expander className="about__expander" title="Fiabilité">
+          <div>
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum
+            atque, eos deleniti laboriosam quis id omnis expedita dolore et?
+            Deserunt aperiam beatae eveniet tenetur error quod magni voluptates
+            saepe accusamus."
+          </div>
+        </Expander>
+
+        <Expander className="about__expander" title="Respect">
+          <div>
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum
+            atque, eos deleniti laboriosam quis id omnis expedita dolore et?
+            Deserunt aperiam beatae eveniet tenetur error quod magni voluptates
+            saepe accusamus."
+          </div>
+        </Expander>
+
+        <Expander className="about__expander" title="Service">
+          <div>
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum
+            atque, eos deleniti laboriosam quis id omnis expedita dolore et?
+            Deserunt aperiam beatae eveniet tenetur error quod magni voluptates
+            saepe accusamus."
+          </div>
+        </Expander>
+
+        <Expander className="about__expander" title="Sécurité">
+          <div>
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum
+            atque, eos deleniti laboriosam quis id omnis expedita dolore et?
+            Deserunt aperiam beatae eveniet tenetur error quod magni voluptates
+            saepe accusamus."
+          </div>
+        </Expander>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
